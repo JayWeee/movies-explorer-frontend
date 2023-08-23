c0d7440ee406cb163513c5d5feacd650e47a1397
JayWee <tylykin2013@yandex.ru>
2023-08-23 16:20:58 +0300

    Добавить компоненты

--- src/components/NavTab/NavTab.js
+++ src/components/NavTab/NavTab.js
@@ -3,11 +3,11 @@ import './NavTab.css';
 
 function NavTab() {
   return (
-    <div className='nav-tab'>
-      <a className='nav-tab__link' href='#project'>О проекте</a>
-      <a className='nav-tab__link' href='#techs'>Технологии</a>
-      <a className='nav-tab__link' href='#about'>Студент</a>
-    </div>
+    <ul className='nav-tab'>
+      <li className='nav-tab__item'><a className='nav-tab__link' href='#project'>О проекте</a></li>
+      <li className='nav-tab__item'><a className='nav-tab__link' href='#techs'>Технологии</a></li>
+      <li className='nav-tab__item'><a className='nav-tab__link' href='#about'>Студент</a></li>
+    </ul>
   );
 }
 
