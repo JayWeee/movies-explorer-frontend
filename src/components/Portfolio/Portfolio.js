a417f9b21cbba24794429ac114a795523669db8d
JayWee <tylykin2013@yandex.ru>
2023-09-06 09:47:05 +0300

    Сделать фдаптив главной страницы под 320

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Portfolio.css';
 import Container from '../Container/Container';
+import arrow from '../../images/link_arrow.svg';
 
 function Portfolio() {
   return (
@@ -11,46 +12,19 @@ function Portfolio() {
           <li className="portfolio__item">
             <a className='portfolio__link' href='https://github.com/JayWeee/how-to-learn' target='_blank' rel='noreferrer'>
               Статичный сайт
-              <svg
-                width="18"
-                height="17"
-                viewBox="0 0 18 17"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg">
-               <path
-                d="M2.60653 16.5241L0.944603 14.8622L13.3026 2.48295H3.7571L3.77841 0.181818H17.2656V13.6903H14.9432L14.9645 4.14489L2.60653 16.5241Z"
-                fill="white"/>
-              </svg>
+              <img className='portfolio__link-arrow' src={arrow} alt='Стрелка' />
             </a>
           </li>
           <li className="portfolio__item">
             <a className='portfolio__link' href='https://github.com/JayWeee/russian-travel' target='_blank' rel='noreferrer'>
               Адаптивный сайт
-              <svg
-                width="18"
-                height="17"
-                viewBox="0 0 18 17"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg">
-               <path
-                d="M2.60653 16.5241L0.944603 14.8622L13.3026 2.48295H3.7571L3.77841 0.181818H17.2656V13.6903H14.9432L14.9645 4.14489L2.60653 16.5241Z"
-                fill="white"/>
-              </svg>
+              <img className='portfolio__link-arrow' src={arrow} alt='Стрелка' />
             </a>
           </li>
           <li className="portfolio__item">
             <a className='portfolio__link' href='https://github.com/JayWeee/react-mesto-api-full-gha' target='_blank' rel='noreferrer'>
               Одностраничное приложение
-              <svg
-                width="18"
-                height="17"
-                viewBox="0 0 18 17"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg">
-               <path
-                d="M2.60653 16.5241L0.944603 14.8622L13.3026 2.48295H3.7571L3.77841 0.181818H17.2656V13.6903H14.9432L14.9645 4.14489L2.60653 16.5241Z"
-                fill="white"/>
-              </svg>
+              <img className='portfolio__link-arrow' src={arrow} alt='Стрелка' />
             </a>
           </li>
         </ul>
